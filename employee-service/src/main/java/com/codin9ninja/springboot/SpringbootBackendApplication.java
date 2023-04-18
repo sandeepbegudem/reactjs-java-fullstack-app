186465541a27af6a0707c42a787f757660609c64
Sandeep Kumar B <124546985+Codin9Ninja@users.noreply.github.com>
2023-04-18 18:55:58 -0500

    add employee service

---
+++ employee-service/src/main/java/com/codin9ninja/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codin9ninja.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
